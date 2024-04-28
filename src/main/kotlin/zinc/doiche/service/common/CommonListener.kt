5974af7ecbd14ba0d7ad07ea1cb67098a6f36820
lc0709 <3dollee@gmail.com>
2024-04-28 23:02:21 +0900

    Feat: Second Cache

--- src/main/kotlin/zinc/doiche/service/common/CommonListener.kt
+++ src/main/kotlin/zinc/doiche/service/common/CommonListener.kt
@@ -10,16 +10,29 @@ import zinc.doiche.Main.Companion.plugin
 import zinc.doiche.lib.annotation.ListenerRegistry
 import zinc.doiche.lib.log.LoggerUtil
 import zinc.doiche.lib.log.LoggerUtil.prefixed
+import zinc.doiche.service.user.UserService
+import zinc.doiche.service.user.`object`.QUser.user
+import zinc.doiche.service.user.repository.UserRepository
+import zinc.doiche.service.user.user
 
 @ListenerRegistry
 class CommonListener: Listener {
     @EventHandler
     fun onReload(event: ServerLoadEvent) {
-//        if(event.type == ServerLoadEvent.LoadType.RELOAD) {
-//            plugin.server.broadcast(prefixed("Reload User Data..."))
-//            Bukkit.getOnlinePlayers().forEach { player ->
-//
-//            }
-//        }
+        if(event.type == ServerLoadEvent.LoadType.RELOAD) {
+            plugin.server.broadcast(prefixed("Reload User Data..."))
+
+            val uuidList = Bukkit.getOnlinePlayers().map { it.uniqueId }
+            val idMap = plugin.query
+                .select(user.id, user.uuid)
+                .from(user)
+                .where(user.uuid.`in`(uuidList))
+                .fetch()
+            idMap.forEach { tuple ->
+                val id = tuple.get(user.id)
+                val uuid = tuple.get(user.uuid)
+                UserService.repository.saveID(uuid!!, id!!)
+            }
+        }
     }
 }
