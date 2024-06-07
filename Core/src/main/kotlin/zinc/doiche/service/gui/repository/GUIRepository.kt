36bb99b572d1584fe82eb1a44567fe087bcc0de3
dev-zinc <zchan1325@gmail.com>
2024-06-07 13:35:02 +0900

    Feat: GUI cache

--- Core/src/main/kotlin/zinc/doiche/service/gui/repository/GUIRepository.kt
+++ Core/src/main/kotlin/zinc/doiche/service/gui/repository/GUIRepository.kt
@@ -2,17 +2,32 @@ package zinc.doiche.service.gui.repository
 
 import zinc.doiche.database.repository.CachedKeyRepository
 import zinc.doiche.service.gui.entity.GUI
+import zinc.doiche.service.gui.entity.QGUI.gUI
 
 class GUIRepository(override val prefix: String) : CachedKeyRepository<String, GUI>() {
     override fun save(entity: GUI) {
-        TODO("Not yet implemented")
+        entityManager.persist(entity)
     }
 
     override fun findById(id: Long): GUI? {
-        TODO("Not yet implemented")
+        return entityManager.find(GUI::class.java, id)
     }
 
     override fun delete(entity: GUI) {
-        TODO("Not yet implemented")
+        entityManager.remove(entity)
+    }
+
+    fun findByName(name: String): GUI? = query
+        .selectFrom(gUI)
+        .where(gUI.name.eq(name))
+        .fetchOne()
+
+    fun findCacheByName(name: String): GUI? {
+        return getId(name)?.let { id ->
+            findById(id)
+        } ?: findByName(name)?.let { gui ->
+            saveId(gui.name, gui.id!!)
+            gui
+        }
     }
 }
