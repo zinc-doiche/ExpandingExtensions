8dc0cfe4540075debe2837672a77a8f5a40baa09
doiche <3dollee@gmail.com>
2024-08-04 12:29:22 +0900

    Feat: add Socket system

--- build.gradle.kts
+++ build.gradle.kts
@@ -154,7 +154,7 @@ applyTo(
             ssh.run(delegateClosureOf<RunHandler> {
                 session(server, delegateClosureOf<SessionHandler> {
                     val file = "$projectDir/build/libs/${project.name}-${project.version}-all.jar"
-                    val directory = "/home/minecraft/${project.name}/plugins"
+                    val directory = "/home/minecraft/${rootProject.name}/${project.name}/plugins"
 
                     put(
                         hashMapOf(
